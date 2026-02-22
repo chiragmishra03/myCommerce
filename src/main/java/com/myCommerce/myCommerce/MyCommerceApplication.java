40f372ad341e0d7cab43e485eb86453fbeeb7e3a
chiragmishra03 <chirag3102001@gmail.com>
2026-02-22 23:31:32 +0530

    "initial setup"

---
+++ src/main/java/com/myCommerce/myCommerce/MyCommerceApplication.java
@@ -0,0 +1,12 @@
+package com.myCommerce.myCommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCommerceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MyCommerceApplication.class, args);
+	}
+
+}
